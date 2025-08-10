960645256fd1cd96a4da83ed090908048b888c3f
altonhudson <ah_1014@hotmail.com>
2025-08-10 13:17:41 -0400

    Send Data Using POST

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 
-document.getElementById('fetch-data').addEventListener('click', function(){
+document.getElementById('fetch-data').addEventListener('click', function () {
     fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`)
-        }
-        return response.json()
-    })
-    .then(data => {
-        console.log(data);
-        displayData(data.title, data.body)
-    })
-    .catch(error => {
-        console.error('Fetch error:', error);
-        errorMessage("Error fetching data, please try again")
-    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+            displayData(data.title, data.body)
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+            errorMessage("Error fetching data, please try again")
+        })
 })
 
 let displayDataDiv = document.querySelector(".display-data")
-function displayData(title, body){
+function displayData(title, body) {
     let h1 = document.createElement('h1')
     h1.innerText = title
     let dataBody = document.createElement('p')
@@ -27,19 +27,19 @@ function displayData(title, body){
     displayDataDiv.appendChild(dataBody)
 }
 
-function errorMessage(msg){
+function errorMessage(msg) {
     let h1 = document.createElement('h1')
     h1.innerText = msg
     displayDataDiv.appendChild(h1)
 }
 
-document.getElementById('XHR-data').addEventListener('click', function() {
+document.getElementById('XHR-data').addEventListener('click', function () {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2', true);
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) { 
-            if (xhr.status === 200) { 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
                 console.log(data);
                 displayData(data.title, data.body);
@@ -52,3 +52,41 @@ document.getElementById('XHR-data').addEventListener('click', function() {
 
     xhr.send();
 });
+
+function successMessage(msg) {
+    let h1 = document.createElement('h1')
+    h1.innerText = msg
+    displayDataDiv.appendChild(h1)
+}
+
+let addTitle = document.querySelector('.add-title')
+let addBody = document.querySelector('.add-body')
+
+document.getElementById('btn-post').addEventListener('click', function (e) {
+    e.preventDefault()
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: addTitle.value,
+            body: addBody.value
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+            successMessage('Post created Successfully!')
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            errorMessage('Error: Message was not posted, please try again.')
+        })
+        
+    });
